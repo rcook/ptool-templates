c03ddba61d0a568f886d26e8e30fe7ea6a29b585
Richard Cook <rcook@rcook.org>
2017-09-07 07:58:10 -0700

    Create universal scripts

--- script/project.cmd
+++ script/project.cmd
@@ -1,2 +1,21 @@
+:<<"::CMDLITERAL"
+@echo off
+goto :CMDSCRIPT
+::CMDLITERAL
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+script_dir=$(cd $(dirname $0); pwd -P)
+
+$script_dir/bootstrap $script_dir/project.py $*
+
+exit $?
+
+:CMDSCRIPT
 @echo off
 call "%~dp0bootstrap.cmd" "%~dp0project.py" %*
+if errorlevel 1 (
+    echo script failed
+    exit /b 1
+)
