1466aec6026db375ddb5b0ca760c708a74bcd8b5
Richard Cook <rcook@rcook.org>
2017-09-14 12:17:18 -0700

    Add pthread support for Linux

---
+++ c++14/cmake/threads.cmake
@@ -0,0 +1,14 @@
+############################################################
+# Copyright (C) 2017, Richard Cook. All rights reserved.
+############################################################
+
+find_package(Threads REQUIRED)
+
+function(add_threads target)
+    if(THREADS_HAVE_PTHREAD_ARG)
+        target_compile_options($${target} PUBLIC "-pthread")
+    endif()
+    if(CMAKE_THREAD_LIBS_INIT)
+        target_link_libraries($${target} "$${CMAKE_THREAD_LIBS_INIT}")
+    endif()
+endfunction()
