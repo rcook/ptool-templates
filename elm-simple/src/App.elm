70f67ebb49f637aefafc4e3f64d4ee9e8ffef065
Richard Cook <rcook@rcook.org>
2017-09-27 09:27:48 -0700

    Simple Elm project

---
+++ elm-simple/src/App.elm
@@ -0,0 +1,58 @@
+$elm_copyright
+module App exposing (..)
+
+import Html exposing (Html, text, div, img)
+import Html.Attributes exposing (src)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { message : String
+    , logo : String
+    }
+
+
+init : String -> ( Model, Cmd Msg )
+init path =
+    ( { message = "$project_name is working!", logo = path }, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ img [ src model.logo ] []
+        , div [] [ text model.message ]
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program String Model Msg
+main =
+    Html.programWithFlags
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
