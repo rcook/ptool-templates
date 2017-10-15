092f78557920e6495625ee0d10727663e41eebf8
Richard Cook <rcook@rcook.org>
2017-10-15 15:44:58 -0700

    Example non-public class

--- java-maven/App.java
+++ java-maven/App.java
@@ -1,6 +1,30 @@
 {{java_copyright}}
 package {{reversed_domain}}.app;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+final class Container
+{
+    public Container()
+    {
+    }
+
+    public void addName(String name)
+    {
+        _names.add(name);
+    }
+
+    public List<String> names()
+    {
+        return _namesImmutable;
+    }
+
+    private ArrayList<String> _names = new ArrayList<String>();
+    private List<String> _namesImmutable = Collections.unmodifiableList(_names);
+}
+
 /**
  * App class
  */
@@ -9,5 +33,15 @@ public final class App
     public static void main(String[] args)
     {
         System.out.println("Hello from {{project_name}}");
+
+        Container items = new Container();
+        items.addName("One");
+        items.addName("Two");
+        items.addName("Three");
+
+        for (String name : items.names())
+        {
+            System.out.println("name=" + name);
+        }
     }
 }
