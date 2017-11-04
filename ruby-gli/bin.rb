b10c52bb957f5e5faa83a9a1dc8646794d394f5f
Richard Cook <rcook@rcook.org>
2017-11-04 14:05:06 -0700

    Use strict argument mode

--- ruby-gli/bin.rb
+++ ruby-gli/bin.rb
@@ -5,6 +5,9 @@ require '{{project_name}}'
 
 include GLI::App
 
+arguments :strict
+subcommand_option_handling :normal
+
 program_desc 'Sample application'
 
 desc 'To-do list file name'
@@ -17,17 +20,14 @@ end
 desc 'Add to-do item'
 arg :text
 command :add do |c|
-  c.action do |global_options, options, args|
-    help_now! 'Specify exactly one argument' if args.count != 1
-    text = args.shift
+  c.action do |global_options, options, (text)|
     $to_do_list.add_item(text)
   end
 end
 
 desc 'List to-do items'
 command :list do |c|
-  c.action do |global_options, options, args|
-    help_now! 'Command takes no arguments' if args.count != 0
+  c.action do |global_options, options|
     $to_do_list.items.each do |item|
       printf("%5d - %s\n", item.id, item.text)
     end
@@ -35,10 +35,10 @@ command :list do |c|
 end
 
 desc 'Mark to-do item as done'
+arg :id
 command :done do |c|
-  c.action do |global_options, options, args|
-    help_now! 'Specify exactly one argument' if args.count != 1
-    id = args.shift.to_i
+  c.action do |global_options, options, (id_s)|
+    id = id_s.to_i
     $to_do_list.items.each do |item|
       $to_do_list.mark_done(item) if item.id == id
     end
