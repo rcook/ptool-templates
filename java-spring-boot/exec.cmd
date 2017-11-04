2786512d6463807cd73bffd072d84ab830dcf4a9
Richard Cook <rcook@rcook.org>
2017-11-03 19:13:21 -0700

    Create universal scripts and symlinks

--- java-spring-boot/exec.cmd
+++ java-spring-boot/exec.cmd
@@ -1,3 +1,18 @@
+{% extends "_shared/universal_script.cmd" %}
+
+{% block bash %}
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+this_dir=$(cd $(dirname $0); pwd -P)
+cd $this_dir
+args=(${@// /\\ })
+mvn exec:java -Dexec.mainClass="{{reversed_domain}}.App" -Dexec.args="${args[*]}"
+exit $?
+{% endblock %}
+
+{% block cmd %}
 @echo off
 setlocal
 set args=%*
@@ -9,3 +24,4 @@ set this_dir=%~f1
 cd /d "%this_dir%"
 mvn exec:java -Dexec.mainClass="{{reversed_domain}}.App" -Dexec.args="%args%" || exit /b 1
 goto :eof
+{% endblock %}
