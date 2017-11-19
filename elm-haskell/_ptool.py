639780a8cd5264dc74657efe16f01927e6b5c66a
Richard Cook <rcook@rcook.org>
2017-11-19 10:54:20 -0800

    Simplify code

--- elm-haskell/_ptool.py
+++ elm-haskell/_ptool.py
@@ -1,7 +1,3 @@
-def _make_filter(ctx, body):
-    b = eval(body)
-    return lambda *args, **kwargs: b(ctx, *args, **kwargs)
-
 def _to_env_project_name(ctx, s):
     return "_".join(map(lambda s: s.upper(), ctx.tokenize(s)))
 
@@ -12,11 +8,10 @@ def _to_paths_module_name(ctx, s):
     return "Paths_{}".format("_".join(map(lambda s: s.lower(), ctx.tokenize(s))))
 
 def ptool_register(ctx):
-    ctx.filters["child_module_name"] = _make_filter(
-        ctx,
-        "lambda ctx, s, module_suffix=\"\": \"{}{}.{}\".format(ctx.module_name, module_suffix, s)")
-
     project_name = ctx.globals["project_name"]
     ctx.globals["env_project_name"] = _to_env_project_name(ctx, project_name)
     ctx.globals["module_name"] = _to_module_name(ctx, project_name)
     ctx.globals["paths_module_name"] = _to_paths_module_name(ctx, project_name)
+
+    ctx.filters["child_module_name"] = \
+        lambda s, module_suffix="": "{}{}.{}".format(ctx.module_name, module_suffix, s)
