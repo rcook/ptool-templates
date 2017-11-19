d96224d093d5b5901b2560b12949f83d87284202
Richard Cook <rcook@rcook.org>
2017-11-19 11:29:37 -0800

    Refactor java-web-app-gradle

---
+++ java-web-app-gradle/_ptool.py
@@ -0,0 +1,9 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", )))
+from _ptool.util import *
+
+def ptool_register(ctx):
+    java_domain = ctx.java_domain
+    ctx.globals["domain_file_path"] = domain_file_path(java_domain)
+    ctx.globals["reversed_domain"] = reversed_domain(java_domain)
