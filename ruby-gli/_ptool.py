288bd5dfa91693f0779b88871d1b932b31f75a24
Richard Cook <rcook@rcook.org>
2017-12-08 15:44:11 -0800

    Fix ruby-gli

---
+++ ruby-gli/_ptool.py
@@ -0,0 +1,12 @@
+##################################################
+# Copyright (C) 2017, All rights reserved.
+##################################################
+
+from ptoollib.util import *
+
+def ptool_register(ctx):
+    pass
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
